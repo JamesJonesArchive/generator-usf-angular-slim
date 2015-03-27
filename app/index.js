219d57ccc385bd511b0242d44062992251f96fd7
James Jones <jamjon3@gmail.com>
2015-03-27 15:17:36 -0400

    Restoring some earlier code

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
+var USFAngularSlimGenerator = module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
   },
